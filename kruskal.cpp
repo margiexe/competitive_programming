870e593ebbc7b79e7036f7f0428a708187e51d37
margi yadav <118652991+margiexe@users.noreply.github.com>
2024-03-13 00:09:28 +0530

    Update kruskal.cpp

--- kruskal.cpp
+++ kruskal.cpp
@@ -15,49 +15,59 @@ const ll INF = 1e16;
 #define debug(...)
 #endif
 
-class DSU{
-   public:
-   vector<int> parent,rank;
-   DSU(int n){
+class DSU
+{
+public:
+   vector<int> parent, rank;
+   DSU(int n)
+   {
       parent.resize(n);
       rank.resize(n);
-      for(int i=0; i<n; i++){
+      for (int i = 0; i < n; i++)
+      {
          parent[i] = i;
          rank[i] = 0;
       }
    }
-   int find(int a){
-      if(parent[a]==a) return a;
+   int find(int a)
+   {
+      if (parent[a] == a)
+         return a;
       return parent[a] = find(parent[a]);
    }
-   void unionset(int a, int b){
+   void unionset(int a, int b)
+   {
       a = find(a);
       b = find(b);
-      if(a==b){
+      if (a == b)
+      {
          return;
       }
-      if(rank[a]>rank[b]) swap(rank[a],rank[b]);
+      if (rank[a] > rank[b])
+         swap(rank[a], rank[b]);
       parent[a] = b;
       rank[b]++;
    }
 };
 
-void kruskal(vector<pair<ll, ii>> &adj)
+void kruskal(vector<pair<ll, ii>> &adj, int n)
 {
-   int n = adj.size();
    DSU ds(n);
-   sort(adj.begin(),adj.end());
-   int mst_cost=0;
-   for(auto x:adj){
+   sort(adj.begin(), adj.end());
+   int mst_cost = 0;
+   for (auto x : adj)
+   {
       int n1 = x.second.first;
       int n2 = x.second.second;
       int weight = x.first;
-      if(ds.find(n1)!=ds.find(n2)){
+      if (ds.find(n1) != ds.find(n2))
+      {
          mst_cost += weight;
-         ds.unionset(n1,n2);
+         ds.unionset(n1, n2);
       }
+      debug(n1,n2,weight);
    }
-   cout<<mst_cost<<endl;
+   cout << mst_cost << endl;
 }
 
 int main()
@@ -72,17 +82,19 @@ int main()
    cin >> t;
    while (t--)
    {
-      int n;
-      cin >> n;
-      vector<ii> a(n);
+      int n,m;
+      cin >> n>>m;
+      vector<ii> a(m);
       int w;
       vector<pair<ll, ii>> adj;
-      for (int i = 0; i < n; i++)
+      for (int i = 0; i < m; i++)
       {
-         cin >> a[i].first >> a[i].second>>w;
-         adj.push_back({w,{a[i].first,a[i].second}});
+         cin >>a[i].first >> a[i].second>>w;
+         adj.push_back({w, {a[i].first-1, a[i].second-1}});
+         
       }
-      kruskal(adj);
+      debug(adj);
+      kruskal(adj,n);
    }
    return 0;
 }
