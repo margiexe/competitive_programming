23cef687037f3fac5bd4d2ac9cb67ca61e4d2c06
margi yadav <118652991+margiexe@users.noreply.github.com>
2024-03-17 18:32:40 +0530

    Create edmonds_karp.cpp

---
+++ edmonds_karp.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<long long> vll;
+typedef vector<int> vi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef tuple<ll, ll, ll> iii;
+
+const ll INF = 1e16;
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+class max_flow
+{
+private:
+   int V;
+   vector<iii> EL;
+   vector<vi> AL;
+   vi d, last;
+   vector<ii> p;
+
+public:
+   max_flow(int initialV) : V(initialV)
+   {
+      EL.clear();
+      AL.assign(V, vi());
+   }
+   // in case of bidirectional set directed = false
+   void add_edge(ll u, ll v, ll w, bool directed = true)
+   {
+      if (u == v)
+      {
+         return; // no self loops
+      }
+      EL.push_back({v, w, 0}); // u --> v, capicity w, flow 0
+      AL[u].push_back(EL.size() - 1);
+      EL.push_back({u, directed ? 0 : w, 0}); // back edge
+      AL[v].push_back(EL.size() - 1);         // remember this edge
+   }
+   bool BFS(ll s, ll t)
+   {
+      d.assign(V, -1);
+      d[s] = 0;
+      p.assign(V, {-1, -1});
+      queue<ll> q({s});
+      while (!q.empty())
+      {
+         ll u = q.front();
+         q.pop();
+         if (u == t)
+            break;
+         for (auto &idx : AL[u])
+         {
+            auto &[v, cap, flow] = EL[idx];
+            if ((cap - flow > 0) && (d[v] == -1)) // positive residual + unexplored
+            {
+               d[v] = d[u] + 1;
+               q.push(v);
+               p[v] = {u, idx}; // parent info
+            }
+         }
+      }
+      return d[t]!=-1;
+   }
+   ll send_one_flow(ll s, ll t, ll f = INF) // send one flow from s-->t
+   {
+      if (s == t)
+         return f; // bottleneck edge f found
+      auto &[u, idx] = p[t];
+      auto &cap = get<1>(EL[idx]), &flow = get<2>(EL[idx]);
+
+      ll pushed = send_one_flow(s, u, min(f, cap - flow));
+      flow += pushed;
+
+      auto &rflow = get<2>(EL[idx ^ 1]); // to get partner edge
+      rflow -= pushed;
+
+      return pushed;
+   }
+   ll edmonds_karp(ll s, ll t)
+   {
+      ll maxf = 0;
+      while (BFS(s, t))
+      {
+         ll f = send_one_flow(s, t);
+         if (f == 0)
+            break;
+         maxf += f;
+      }
+      return maxf;
+   }
+};
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   freopen("error.txt", "w", stderr);
+#endif
+   ios_base::sync_with_stdio(false);
+   // int t = 1;
+   // cin >> t;
+   // while (t--)
+   // {
+   ll n, m;
+   cin >> n;
+   vll a(n);
+   for (int i = 0; i < n; i++)
+   {
+      cin >> a[i];
+   }
+   cin >> m;
+   vll b(m);
+   for (int i = 0; i < m; i++)
+   {
+      cin >> b[i];
+   }
+   max_flow maxf(n + m + 2);
+   for (int i = 0; i < n; i++)
+   {
+      for (int j = 0; j < m; j++)
+      {
+         if (abs(a[i] - b[j]) <= 1)
+         {
+            maxf.add_edge(i + 1, n + j + 1, 1);
+         }
+      }
+   }
+   for (int i = 1; i <= n; i++)
+   {
+      maxf.add_edge(0, i, 1);
+   }
+   for (int i = 1; i <= m; i++)
+   {
+      maxf.add_edge(n + i, n + m + 1, 1);
+   }
+   cout << maxf.edmonds_karp(0,n+m+1)<<endl;
+           // }
+           return 0;
+}
