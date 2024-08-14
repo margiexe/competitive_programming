a90a098e5596a803174358db6fec998564d95901
margi yadav <118652991+margiexe@users.noreply.github.com>
2024-08-14 12:01:22 +0530

    Update min_heap.cpp

--- min_heap.cpp
+++ min_heap.cpp
@@ -9,89 +9,105 @@ typedef long long ll;
 #define debug(...)
 #endif
 
-class MinHeap{
-    private:
-
+class MinHeap {
+private:
     vector<int> heap;
 
-    int parent(int index){
-        return (index-1)/2;
+    int parent(int index) {
+        return (index - 1) / 2;
     }
 
-    int left(int index){
-        return index*2+1;
+    int left(int index) {
+        return index * 2 + 1;
     }
 
-    int right(int index){
-        return index*2+2;
+    int right(int index) {
+        return index * 2 + 2;
     }
 
-    void swap(int i, int j){
-        int temp = heap[i]; 
+    void swap(int i, int j) {
+        int temp = heap[i];
         heap[i] = heap[j];
         heap[j] = temp;
     }
 
-    void upheap(int index){
-        if(index==0) return;
+    void upheap(int index) {
+        if (index == 0) return;
         int p = parent(index);
-        if(heap[index]<heap[p]){
-            swap(index,p);
+        if (heap[index] < heap[p]) {
+            swap(index, p);
             upheap(p);
         }
     }
 
-    void downheap(int index){
+    void downheap(int index) {
         int l = left(index);
         int r = right(index);
         int small = index;
-        if(l<heap.size() && heap[l]<heap[small]){
+        if (l < heap.size() && heap[l] < heap[small]) {
             small = l;
         }
-        if(r<heap.size() && heap[r]<heap[small]){
+        if (r < heap.size() && heap[r] < heap[small]) {
             small = r;
         }
-        if(small!=index){
-            swap(index,small);
+        if (small != index) {
+            swap(index, small);
             downheap(small);
         }
     }
 
-    public:
-
+public:
     MinHeap() {}
 
-    void insert(int num){
+    void insert(int num) {
         heap.push_back(num);
-        upheap(heap.size()-1);
+        upheap(heap.size() - 1);
     }
 
-    void removemin(){
-        if(heap.empty()){
+    void removemin() {
+        if (heap.empty()) {
             return;
         }
-        int mini = heap[0];
         heap[0] = heap.back();
         heap.pop_back();
-        if(!heap.empty()) downheap(0);
+        if (!heap.empty()) downheap(0);
     }
 
-    int getmin(){
-        if(heap.size()==0){
+    int getmin() {
+        if (heap.size() == 0) {
             return 0;
         }
         return heap[0];
     }
 
-    bool isempty(){
-        return heap.size()==0;
+    bool isempty() {
+        return heap.size() == 0;
     }
 
-    void print(){
-        for(auto &x:heap){
-            cout<<x<<" ";
+    void print() {
+        for (auto &x : heap) {
+            cout << x << " ";
         }
-        cout<<endl;
+        cout << endl;
+    }
+
+    void remove(int num) {
+        auto it = find(heap.begin(), heap.end(), num);
+        
+        if (it == heap.end()) {
+            cout << "Element not found in heap" << endl;
+            return;
+        }
+
+        int index = it - heap.begin(); 
+
+        heap[index] = heap.back();
+        heap.pop_back(); 
+
+        if (index < heap.size()) {
+            upheap(index);
+            downheap(index);
+        }
     }
 };
 
@@ -104,9 +120,11 @@ void solve() {
     h.insert(1);
     h.insert(10);
     h.print();
-    cout<<h.getmin()<<endl;
+    cout << h.getmin() << endl;
     h.isempty();
     h.removemin();
+    h.print();
+    h.remove(5);
     h.print();
 }
 
@@ -118,10 +136,10 @@ int main() {
 #endif
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    ll t=1;
-    // cin >> t;
+    ll t = 1;
     while (t--) {
         solve();
     }
     return 0;
 }
+
